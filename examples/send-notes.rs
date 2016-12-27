e5403440dbe8346bc12ebd3513e865b5b13a2eec
Christian Perez Llamas <christian.perez@schibsted.com>
2016-12-27 18:22:48 +0100

    send-notes example refactored to make it cleaner

--- examples/send-notes.rs
+++ examples/send-notes.rs
@@ -3,16 +3,16 @@ extern crate coremidi_sys;
 extern crate time;
 extern crate libc;
 
-use core_foundation::string::{CFString, CFStringRef, CFStringGetLength, UniChar};
-use core_foundation::base::{TCFType, CFIndex, CFRelease};
+use core_foundation::string::{CFString, CFStringRef};
+use core_foundation::base::{TCFType};
 
 use coremidi_sys::{
     MIDIGetNumberOfDestinations, MIDIGetDestination,
     MIDIObjectGetStringProperty, kMIDIPropertyDisplayName,
     MIDIPacketList, MIDIPacket,
     MIDIClientRef, MIDIClientCreate, MIDIClientDispose,
-    MIDIPortRef, MIDIOutputPortCreate,
-    MIDIEndpointRef, MIDISend, ItemCount };
+    MIDIPortRef, MIDIOutputPortCreate, MIDIPortDispose,
+    MIDIEndpointRef, MIDISend, ItemCount, UInt16};
 
 use std::time::Duration;
 use std::thread;
@@ -22,30 +22,28 @@ use std::env;
 
 fn main() {
     let destination_index = get_destination_index();
-
     println!("Destination index: {}", destination_index);
 
-    let client = create_client("example-client");
+    let client = create_client("example-client")
+        .expect("Failed to create a client");
 
-    let output_port = create_output_port(client, "example-port");
+    let output_port = create_output_port(client, "example-port")
+        .expect("Failed to create an output port");
 
-    let endpoint: MIDIEndpointRef = unsafe { MIDIGetDestination(destination_index) };
+    let endpoint = get_destination(destination_index);
 
-    for _ in 0..10 {
-        println!("Sending note ...");
+    for i in 0..10 {
+        println!("[{}] Sending note ...", i);
 
-        let packet_list = create_note_on(0, 64, 127);
-        let status = unsafe { MIDISend(output_port, endpoint, &packet_list) };
-        assert!(status == 0, "Failed to send a packet list");
+        send(output_port, endpoint, create_note_on(0, 64, 127));
 
         thread::sleep(Duration::from_millis(1000));
 
-        let packet_list = create_note_off(0, 64, 127);
-        let status = unsafe { MIDISend(output_port, endpoint, &packet_list) };
-        assert!(status == 0, "Failed to send a packet list");
+        send(output_port, endpoint, create_note_off(0, 64, 127));
     }
 
-    unsafe { MIDIClientDispose(client) };
+    dispose_port(output_port);
+    dispose_client(client);
 }
 
 fn get_destination_index() -> ItemCount {
@@ -89,7 +87,22 @@ fn print_destinations() {
     }
 }
 
-fn create_client(name: &str) -> MIDIClientRef {
+fn get_display_name(endpoint: MIDIEndpointRef) -> Option<String> {
+    let mut display_name_ref: CFStringRef = unsafe { mem::uninitialized() };
+    let status = unsafe { MIDIObjectGetStringProperty(
+        endpoint,
+        kMIDIPropertyDisplayName,
+        &mut display_name_ref) };
+    if status == 0 {
+        let display_name: CFString = unsafe { TCFType::wrap_under_create_rule(display_name_ref) };
+        Some(format!("{}", display_name))
+    }
+    else {
+        None
+    }
+}
+
+fn create_client(name: &str) -> Option<MIDIClientRef> {
     let client_name = CFString::new(name);
     let mut client: MIDIClientRef = unsafe { mem::uninitialized() };
     let status = unsafe { MIDIClientCreate(
@@ -97,12 +110,10 @@ fn create_client(name: &str) -> MIDIClientRef {
         None, ptr::null_mut(),
         &mut client)
     };
-    assert!(status == 0, "Failed to create a client");
-
-    client
+    if status == 0 { Some(client) } else { None }
 }
 
-fn create_output_port(client: MIDIClientRef, name: &str) -> MIDIPortRef {
+fn create_output_port(client: MIDIClientRef, name: &str) -> Option<MIDIPortRef> {
     let output_port_name = CFString::new(name);
     let mut output_port: MIDIPortRef = unsafe { mem::uninitialized() };
     let status = unsafe { MIDIOutputPortCreate(
@@ -110,48 +121,44 @@ fn create_output_port(client: MIDIClientRef, name: &str) -> MIDIPortRef {
         output_port_name.as_concrete_TypeRef(),
         &mut output_port)
     };
-    assert!(status == 0, "Failed to create an output port for {}", output_port_name);
-
-    output_port
+    if status == 0 { Some(output_port) } else { None }
 }
 
-fn create_note_on(channel: u8, note: u8, velocity: u8) -> MIDIPacketList {
-    let mut packet = MIDIPacket { timeStamp: 0, length: 3, data: [0; 256] };
-    packet.data[0] = 0x90 | (channel & 0x0f);
-    packet.data[1] = note & 0x7f;
-    packet.data[2] = velocity & 0x7f;
+fn get_destination(index: ItemCount) -> MIDIEndpointRef {
+    unsafe { MIDIGetDestination(index) }
+}
 
+fn create_note_on(channel: u8, note: u8, velocity: u8) -> Vec<u8> {
+    vec![
+        0x90 | (channel & 0x0f),
+        note & 0x7f,
+        velocity & 0x7f]
+}
+
+fn create_note_off(channel: u8, note: u8, velocity: u8) -> Vec<u8> {
+    vec![
+        0x80 | (channel & 0x0f),
+        note & 0x7f,
+        velocity & 0x7f]
+}
+
+fn create_packet_list(data: Vec<u8>) -> MIDIPacketList {
+    let len = data.len();
+    let mut packet = MIDIPacket { timeStamp: 0, length: len as UInt16, data: [0; 256] };
+    packet.data[0..len].clone_from_slice(&data);
     MIDIPacketList { numPackets: 1, packet: [packet]}
 }
 
-fn create_note_off(channel: u8, note: u8, velocity: u8) -> MIDIPacketList {
-    let mut packet = MIDIPacket { timeStamp: 0, length: 3, data: [0; 256] };
-    packet.data[0] = 0x80 | (channel & 0x0f);
-    packet.data[1] = note & 0x7f;
-    packet.data[2] = velocity & 0x7f;
-
-    MIDIPacketList { numPackets: 1, packet: [packet] }
+fn send(port: MIDIPortRef, endpoint: MIDIEndpointRef, data: Vec<u8>) {
+    let packet_list = create_packet_list(data);
+    let status = unsafe { MIDISend(port, endpoint, &packet_list) };
+    assert!(status == 0, "Failed to send data");
 }
 
-extern {
-    pub fn CFStringGetCharacterAtIndex(theString: CFStringRef, index: CFIndex) -> UniChar;
+fn dispose_port(port: MIDIPortRef) {
+    unsafe { MIDIPortDispose(port) };
 }
 
-fn get_display_name(endpoint: MIDIEndpointRef) -> Option<String> {
-    unsafe {
-        let mut display_name_ref: CFStringRef = mem::uninitialized();
-        match MIDIObjectGetStringProperty(endpoint, kMIDIPropertyDisplayName, &mut display_name_ref) {
-            0 => {
-                let mut name_chars = Vec::<char>::new();
-                let len = CFStringGetLength(display_name_ref);
-                for i in 0..len {
-                    let ch = CFStringGetCharacterAtIndex(display_name_ref, i);
-                    name_chars.push(std::char::from_u32(ch as u32).unwrap());
-                }
-                CFRelease(display_name_ref as *mut libc::c_void);
-                Some(name_chars.iter().cloned().collect::<String>())
-            },
-            _ => None
-        }
-    }
+fn dispose_client(client: MIDIClientRef) {
+    unsafe { MIDIClientDispose(client) };
 }
